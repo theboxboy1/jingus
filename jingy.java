4f3b741ad8d1278df1a0846cfbed6d91c077fbcd
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2023-09-07 13:53:34 -0400

    Create jingy.java

---
+++ jingy.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.print("kumalala");
+    System.out.println("kumalala");
+    System.out.println("savesta");
+    System.out.println(5+6*3-8);
+  }
+}
